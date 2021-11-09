cc3578bc10772ea92fd2ded15749b7e18ebd8b6e
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-11-09 11:47:10 +0530

    feat: feed (share, post components) added

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Post.css";
+import { MoreVert } from "@material-ui/icons";
+
+export const Post = () => {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img src="/assets/person/5.jpeg" className="postProfileImg" />
+            <span className="postUserName">Naveen</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">hello its my post</span>
+          <img src="/assets/post/1.jpeg" className="postImage" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" />
+            <img className="likeIcon" src="/assets/heart.png" />
+            <span className="likeCounter">32 people Like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
