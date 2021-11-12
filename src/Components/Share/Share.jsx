c55f4a0e58c6ac6366cdc2234c9ecb2a8ec7f771
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-11-12 22:34:57 +0530

    bug: file uploading methods improved

--- src/Components/Share/Share.jsx
+++ src/Components/Share/Share.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Share.css";
 import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
 import { useAuth } from "../../Context/AuthContext";
+import axios from "axios";
 
 export const Share = () => {
+  const link = process.env.PORT || "http://localhost:3000/";
+
   const {
     user: { user },
   } = useAuth();
+  const desc = useRef();
+  const [file, setFile] = useState(null);
+  console.log({ file });
 
-  const link = process.env.PORT || "http://localhost:3000/";
   return (
     <div className="share">
       <div className="shareWrapper">
@@ -23,17 +28,53 @@ export const Share = () => {
             alt=""
           />
           <input
-            placeholder="What's in your Mind Naveen?"
+            placeholder={`What's in your Mind ${user.username}?`}
             className="shareInput"
+            ref={desc}
           />
         </div>
         <hr className="shareHr" />
-        <div className="shareBottom">
+        <form
+          className="shareBottom"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const newPost = {
+              userId: user._id,
+              desc: desc.current.value,
+            };
+
+            if (file) {
+              const data = new FormData();
+              const fileName = file.name;
+              data.append("file", file);
+              data.append("name", fileName);
+              newPost.img = `assets/post/${fileName}`;
+              try {
+                await axios.post("/upload", data);
+              } catch (err) {
+                console.log(err.message);
+              }
+            }
+            try {
+              await axios.post("posts/", newPost);
+              window.location.reload();
+            } catch (error) {
+              console.log({ error: error.message });
+            }
+          }}
+        >
           <div className="shareOptions">
-            <div className="shareOption">
+            <label htmlFor="file" className="shareOption">
               <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
-            </div>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="file"
+                accept=".png,.jpeg,.jpg"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
             <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
@@ -47,8 +88,10 @@ export const Share = () => {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
-          <button className="shareBtn">Share</button>
-        </div>
+          <button className="shareBtn" type="submit">
+            Share
+          </button>
+        </form>
       </div>
     </div>
   );
